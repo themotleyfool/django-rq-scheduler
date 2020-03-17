3aacab6649b53bb026d2a5e1324bbdf9050ee69d
Tristan Homsi <tristanhomsi@gmail.com>
2020-03-16 23:14:04 -0400

    whoops actually use the correct version number

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='readwise-django-rq-scheduler',
-    version='1.1.4',
+    version='1.2.2',
     description="A database backed job scheduler for Django RQ",
     long_description=long_description,
     packages=find_packages(),
